a3f46639871f276bef985bdbe04a8422fb37345d
Martyn Taylor <mtaylor@redhat.com>
2012-05-24 17:08:43 +0100

    base image work

--- app/models/image_management/base_image.rb
+++ app/models/image_management/base_image.rb
@@ -2,5 +2,11 @@ module ImageManagement
  class BaseImage < ActiveRecord::Base
     has_many :image_versions
     belongs_to :template
+
+    accepts_nested_attributes_for :template
+
+    # def to_xml(options = {})
+      # #super options.merge(:methods => [:template])
+    # end
   end
 end
