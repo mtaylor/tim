1d9e91277f6af1fdc514e1c03df2c913802afb7f
Martyn Taylor <mtaylor@redhat.com>
2012-05-28 11:54:37 +0100

    Added rspec/rails to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
 require File.expand_path("../../lib/image_factory/image_factory.rb",  __FILE__)
-require 'rspec'
+require 'rspec/rails'
 require 'factory_girl'
 
 Dir.glob(File.dirname(__FILE__) + "/factories/*/*").each do |factory|
