c0c18605971be941b8a51ab95310b417e4e4d090
Martyn Taylor <mtaylor@redhat.com>
2012-09-24 18:54:50 +0100

    Created Added User Attributes Filter
    This is a temporary work around to the problem explained here:
    
    https://github.com/mtaylor/Rails-Nested-Resource-Issues

--- lib/image_management.rb
+++ lib/image_management.rb
@@ -1,6 +1,10 @@
 require "image_management/engine"
 require "image_factory/image_factory.rb"
 
+# FIXME Remove filter once support for custom XML with nested resources is
+# supported.
+require "filters/image_management/user_keys_filter"
+
 module ImageManagement
   mattr_accessor :user_class
   mattr_accessor :provider_account_class
